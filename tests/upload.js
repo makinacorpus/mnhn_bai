a35c07ffd91ca6a792cd9787ff1f2b3131b6dca5
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-12-02 13:05:48 +0100

    uploadtest

--- tests/upload.js
+++ tests/upload.js
@@ -7,50 +7,51 @@ var PASSWORD = 'foo';
 var U = 'admin';
 var P = 'admin';
 var casper = require('casper').create({
-    "pageSettings": {"userName": USERNAME, "password": PASSWORD},
     "logLevel": "debug",
-    "waitTimeout": 1000000000,
+    "localToRemoteUrlAccessEnabled" : true,
     "verbose": true});
 
 casper.start(URL);
 
-casper.then(function(){casper.click('a[href="/login"]');});
+function timeout() {this.echo("ERROR -- Timeout !").exit(1);}
 
-casper.then(function(){
-    casper.waitFor(
-        function (){return this.exists('input[name="username"]')},
-        function (){return this.fill('form', {'username': U, 'password': P}, true)},
-        5);
+casper.setHttpAuth(USERNAME, PASSWORD);
+
+casper.then(function enterlog(){casper.click('a[href="/login"]');});
+
+casper.then(function login(){
+    return this.waitFor(
+        function testlog(){return this.exists('input[name="username"]')},
+        function log(){return this.fill('form', {'username': U, 'password': P}, true)},
+        timeout);
 });
 
-casper.then(function(){casper.click('a[href="/gallery/1"]');});
+casper.then(function entergal(){return this.click('a[href="/gallery/1"]');});
 
-casper.then(function(){
-    casper.waitFor(
-        function (){return this.exists('div.card')},
-        function (){return this.click('div.card a')},
-        5);
+casper.then(function gal(){
+    return this.waitFor(
+        function testgal(){return this.exists('div.card')},
+        function entergal(){return this.click('div.card a')},
+        timeout);
 });
 
-casper.then(function(){
-    casper.waitFor(
-        function (){return this.exists('div.edit')},
-        function (){return this.click('div.edit a')},
-        5);
+casper.then(function edit(){
+    return this.waitFor(
+        function testedit(){return this.exists('div.edit')},
+        function enteredit(){return this.click('div.edit a')},
+        timeout);
 });
 
-casper.then(function(){
-    casper.waitFor(
-        function (){return this.exists('#edit_form')},
-        function (){return this.fill('#edit_form',
-                                     {"media_files": '15mo'},
-                                     true);},
-        5);
+casper.then(function mod(){
+    return this.waitFor(
+        function testmod(){return this.exists('select#gallery option[value="1"]')},
+
+        function editmod(){return this.fill('#edit_form', {"gallery": "Galerie principale",
+                                                    "media_files": '15mo'},
+                                     true)},
+        timeout)
 });
 
-casper.then(function(){
-    this.debugHTML();
-    this.exit();
-});
+casper.then(function(){casper.exit(0)});
 
 casper.run();
