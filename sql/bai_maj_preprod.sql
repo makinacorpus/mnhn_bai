a927ade7277f7e11e01a60d3b4b9cf1b04a873d5
Sylvain Beorchia <sbe@makina-corpus.com>
2015-02-20 17:09:39 +0100

    Typo in sql

--- sql/bai_maj_preprod.sql
+++ sql/bai_maj_preprod.sql
@@ -9,4 +9,4 @@ alter table gallery add parent integer;
 -- INSERT INTO gallery (title, "desc", parent, id, "createdAt", "updatedAt") VALUES ('Galerie des cerveaux', 'Galerie des cerveaux', NULL, 2, '2014-10-03 00:00:00+02', '2014-12-18 14:53:26+01');
 
 
-ALTER table ADD acquisition_params_en text;
+ALTER table object3d ADD acquisition_params_en text;
